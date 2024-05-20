9784045c97f0669742533a40ef673908089d3eec
Angela Montanez <angelamountains@gmail.com>
2024-05-20 16:50:48 +0200

    Co-authored-by: Kaiser_Elie <Juniorelie@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+      let randomQuestion = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = randomQuestion;
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    return this.correctAnswers++;
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    } else if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+  }
 }
